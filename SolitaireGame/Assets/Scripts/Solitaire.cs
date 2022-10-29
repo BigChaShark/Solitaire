0d3d9697e8839ec2c6e628e4577d231982b337bf
BigChaShark <gaharax.gaharax@gmail.com>
2022-10-29 10:53:41 +0700

    Add Main Scripts
    1.Solitaire(GameManager)
    2.Selectable
    3.UserInput

---
+++ SolitaireGame/Assets/Scripts/Solitaire.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solitaire : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
+}
